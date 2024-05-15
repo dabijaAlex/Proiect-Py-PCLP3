5376c7a4a1cef355837639d43f2bcaec387ee00e
Dabija Alex <dabijaalex27@gmail.com>
2024-05-15 21:32:53 +0300

    solved up to C5 (not rly polished)

--- Surse/Cerinta1.py
+++ Surse/Cerinta1.py
@@ -4,90 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# def has_duplicate_lines():
-#     with open("train.csv", 'r') as file:
-#         lines = file.readlines()
-#         unique_lines = set(lines)
-#     return len(unique_lines) != len(lines)
-
-
-# def format_line(line):
-#     line = line.strip('\n')
-#     list_of_line = line.split(',')
-#     merged_elem = list_of_line[3] + "," + list_of_line[4]
-#     list_of_line[3:5] = [merged_elem]
-#     return list_of_line
-#     pass
-
-# def check_type(element):
-
-#     tip = "string"
-
-#     try:
-#         float(element)
-#         tip = "float"
-#     except ValueError:
-#         pass
-
-#     try:
-#         int(element)
-#         tip = "int"
-#     except ValueError:
-#         pass
-
-#     return tip
-
-# def get_types_for_each(list_of_line, tipuri_pt_col):
-#     for i in range(len(list_of_line)):
-#         if list_of_line[i] != '':
-#             type_of_data = check_type(list_of_line[i])
-#             tipuri_pt_col[cols[i]] = type_of_data
-#     return tipuri_pt_col
-
-# def count_nr_elems_each_col(list_of_line, nr_elem_pe_col):
-#     for i in range(len(list_of_line)):
-#         if list_of_line[i] != '':
-#             nr_elem_pe_col[i] += 1
-#     return nr_elem_pe_col
-
-# f = open('train.csv', 'r')
-
-# #numaram coloaneale
-# line = f.readline()
-# line = line.strip('\n')
-
-# cols = line.split(',')
-# nr_coloane = len(cols)
-# print("nr coloane = ", nr_coloane)
-
-
-# tipuri_pt_col = {}
-# for i in cols:
-#     tipuri_pt_col[i] = "default"
-
-# nr_lines = 0
-# nr_elem_lipsa_pe_col = [0] * 12
-# line = f.readline()
-
-# while line:
-#     nr_lines += 1
-#     list_of_line = format_line(line)
-#     tipuri_pt_col = get_types_for_each(list_of_line, tipuri_pt_col)
-
-#     nr_elem_lipsa_pe_col = count_nr_elems_each_col(list_of_line, nr_elem_lipsa_pe_col)
-
-#     line = f.readline()
-
-# print(tipuri_pt_col)
-
-# for i in range(len(nr_elem_lipsa_pe_col)):
-#     nr_elem_lipsa_pe_col[i] = nr_lines - nr_elem_lipsa_pe_col[i]
-# print(nr_elem_lipsa_pe_col)
-
-# print("nr linii = ", nr_lines)
-
-# print("Does the file have dupes ?", has_duplicate_lines())
-# f.close()
 
 df = pd.read_csv('train.csv')
 print(df.head())
@@ -106,9 +22,9 @@ print("nr linii = ", num_rows)
 print("\n")
 
 if (df.duplicated().sum()):
-    print("there are some duplicates")
+    print("there are some duplicates\n")
 else:
-    print("no duplicates")
+    print("no duplicates\n")
 
 fig, axs = plt.subplots(1, 3)
 
@@ -126,5 +42,136 @@ axs[2].pie(z, autopct='%1.1f%%', labels = mylabels_gender)
 
 
 plt.tight_layout()
+plt.savefig("img1.png")
+plt.close()
 
-plt.savefig("img.png")
+######################################################
+
+ageDist = df['Age'].values.tolist()
+plt.hist(ageDist)
+plt.title("Age Distribution")
+
+plt.savefig("./../Histograms/ageDist.png")
+plt.close()
+
+############
+
+SurvivedDist = df['Survived'].values.tolist()
+plt.hist(SurvivedDist, bins=[0, 0.5, 1], label = "hop")
+plt.title("Survival Distribution")
+
+
+plt.savefig("./../Histograms/SurvivalDist.png")
+plt.close()
+
+###############
+
+PclassDist = df['Pclass'].values.tolist()
+plt.hist(PclassDist, bins=3)
+plt.title("Pclass Distribution")
+
+plt.savefig("./../Histograms/Pclass.png")
+plt.close()
+
+
+#############
+SibSpDist = df['SibSp'].values.tolist()
+plt.hist(SibSpDist, log = 1)
+plt.title("SibSp Distribution")
+
+plt.savefig("./../Histograms/SibSp.png")
+plt.close()
+
+
+##############
+ParchDist = df['Parch'].values.tolist()
+plt.hist(ParchDist, bins = 3)
+plt.title("Parch Distribution")
+
+plt.savefig("./../Histograms/Parch.png")
+plt.close()
+
+###########
+
+FareDist = df['Fare'].values.tolist()
+plt.hist(FareDist, log = 1)
+plt.title("Fare Distribution")
+
+plt.savefig("./../Histograms/Fare.png")
+plt.close()
+
+#######################################################
+
+
+nan_count = df.isna().sum()
+cols_missing_vals = df.columns[df.isnull().any()]
+
+for i in range(0,len(cols_missing_vals)):
+    print("{:.2f}".format(nan_count[cols_missing_vals[i]] / num_rows * 100),"% din datele din col", cols_missing_vals[i], "lipsesc")
+print("\n")
+
+######
+
+df_survived = df[df['Survived'] == 1]
+nan_count = df_survived.isna().sum()
+cols_missing_vals = df_survived.columns[df_survived.isnull().any()]
+
+for i in range(0,len(cols_missing_vals)):
+    print("{:.2f}".format(nan_count[cols_missing_vals[i]] / num_rows * 100),"% din datele din col", cols_missing_vals[i], "lipsesc in cazul pasagerilor ce au supravietiuit")
+print("\n")
+#####
+
+df_dead = df[df['Survived'] == 0]
+nan_count = df_dead.isna().sum()
+cols_missing_vals = df_survived.columns[df_dead.isnull().any()]
+
+for i in range(0,len(cols_missing_vals)):
+    print("{:.2f}".format(nan_count[cols_missing_vals[i]] / num_rows * 100),"% din datele din col", cols_missing_vals[i], "lipsesc in cazul pasagerilor ce NU au supravietiuit")
+print("\n")
+
+####################################
+
+
+df_0_20 = df[df['Age'] <= 20]
+
+df_21_40 = df[df['Age'] <= 40]
+df_21_40 = df_21_40[df_21_40['Age'] >= 21]
+
+df_41_60 = df[df['Age'] <= 60]
+df_41_60 = df_41_60[df_41_60['Age'] >= 41]
+
+df_61plus = df[df['Age'] >= 61]
+
+print(len(df_0_20), len(df_21_40), len(df_41_60), len(df_61plus))
+
+nan_count = df.isna().sum()
+print(nan_count['Age'])
+
+
+df.insert(1, "AgeGroup", "")
+for i in range(0,num_rows):
+    if df['Age'][i] <= 20:
+        df.loc[i, "AgeGroup"] = 1
+
+    if df['Age'][i] <= 40 and df['Age'][i] >= 21:
+        df.loc[i, "AgeGroup"] = 2
+
+    if df['Age'][i] <= 60 and df['Age'][i] >= 41:
+        df.loc[i, "AgeGroup"] = 3
+
+    if df['Age'][i] >= 61:
+        df.loc[i, "AgeGroup"] = 4
+
+
+a = df['AgeGroup'].value_counts()[1] / num_rows * 100
+b = df['AgeGroup'].value_counts()[2] / num_rows * 100
+c = df['AgeGroup'].value_counts()[3] / num_rows * 100
+d = df['AgeGroup'].value_counts()[4] / num_rows * 100
+
+unknown = 100 - a - b - c - d
+x = np.array([a, b, c, d, unknown])
+mylabels = ["[0-20]", "[21-40]", "[41-60]", "61+", "Nan"]
+plt.pie(x, autopct='%1.1f%%', labels = mylabels)
+
+plt.savefig("C5.png")
+plt.close()
