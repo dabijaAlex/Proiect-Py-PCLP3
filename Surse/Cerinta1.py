52ab61a8f370837c3854460f79b70a8160d32697
Dabija Alex <dabijaalex27@gmail.com>
2024-05-26 22:51:47 +0300

    C9

--- Surse/Cerinta1.py
+++ Surse/Cerinta1.py
@@ -252,7 +252,7 @@ plt.ylabel('Percentages', fontsize = 12)
 plt.bar_label(bars)
 
 plt.savefig("C7.png")
-plt.close
+plt.close()
 
 df_to_complete = pd.read_csv('train.csv')
 
@@ -283,3 +283,93 @@ nan_count = df_to_complete.isna().sum()
 print(nan_count)
 df_to_complete.to_csv('new_df.csv', index = True)
 #intreaba daca trebuie sa cureti datele cu NA
+
+def extract_title(name):
+    if 'Mr.' in name:
+        return 'Mr.'
+    elif 'Mrs.' in name:
+        return 'Mrs.'
+    elif 'Ms.' in name:
+        return 'Ms.'
+    elif 'Don' in name:
+        return 'Don'
+    elif 'Dr.' in name:
+        return 'Dr.'
+    elif 'Miss.' in name:
+        return 'Miss.'
+    elif 'Master.' in name:
+        return 'Master.'
+    elif 'Jonkheer.' in name:
+        return 'Jonkheer.'
+    elif 'Rev.' in name:
+        return 'Rev.'
+    elif 'Mlle.' in name:
+        return 'Mlle'
+    elif 'Countess' in name:
+        return 'Countess'
+    elif 'Capt.' in name:
+        return 'Capt.'
+    elif 'Mme.' in name:
+        return 'Mme.'
+    elif 'Major.' in name:
+        return 'Major.'
+    elif 'Lady.' in name:
+        return 'Lady'
+    elif 'Col.' in name:
+        return 'Col'
+    elif 'Sir.' in name:
+        return 'Sir.'
+    else: return "unknown"
+
+def check_title_sex(title, sex):
+    male_titles = ['Mr.', 'Don', 'Master.', 'Rev.', 'Col.', 'Capt.', 'Major.', 'Sir.']
+    female_titles = ['Mrs.', 'Ms.', 'Miss.', 'Mlle', 'Countess', 'Mme.', 'Lady']
+    if title in male_titles and sex == 'male':
+        return True
+    elif title in female_titles and sex == 'female':
+        return True
+    elif title == 'Dr.':
+        return True
+    elif title == 'Jonkheer.':
+        return True
+    else:
+        return False
+
+df['Title'] = df['Name'].apply(extract_title)
+
+df['Title_Valid'] = df.apply(lambda row: check_title_sex(row['Title'], row['Sex']), axis=1)
+
+title_counts = (df[(df['Title_Valid'] == True)])['Title'].value_counts()
+mylabels = (df[(df['Title_Valid'] == True)])['Title'].value_counts().index
+bars = plt.bar(mylabels, title_counts)
+plt.xticks(rotation = 35)
+plt.autoscale(enable = True, tight=True, axis = 'y')
+plt.xlabel('Titlu')
+plt.ylabel('Număr de persoane')
+plt.title('Numărul de persoane pentru fiecare titlu')
+plt.bar_label(bars)
+plt.savefig("C9.png")
+plt.close()
+
+
+
+
+# x = df['Parch'].value_counts()
+# mylabels = df['Parch'].value_counts().index
+# bars = plt.bar(mylabels, x)
+# plt.title("Parch Distribution")
+# plt.ylabel('number of people', fontsize = 12)
+# plt.bar_label(bars)
+
+
+# plt.savefig("./../Histograms/Parch.png")
+# plt.close()
+
+# ###########
+
+# # FareDist = df['Fare'].values.tolist()
+# bars = plt.hist(df['Fare'], [0, 30, 60, 100, 200, 300, 500, 1000])
+# plt.title("Fare Distribution")
+
+# plt.savefig("./../Histograms/Fare.png")
+# plt.close()
